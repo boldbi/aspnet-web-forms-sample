d16e4f87db3d83e3354f4ddbf413159899b21450
Nithya <nithya.gopal@syncfusion.com>
2023-12-08 16:10:18 +0530

    Restrict-to-pass-serverdetails-to-client-app-aspnet-webforms

--- BoldBIEmbedSample/Site.Master.cs
+++ BoldBIEmbedSample/Site.Master.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BoldBIEmbedSample.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,45 @@ namespace BoldBIEmbedSample
 {
     public partial class SiteMaster : MasterPage
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        //protected void Page_Load(object sender, EventArgs e)
+        //{
+            protected string GetDashboardInfoJson()
+            {
+            DashboardInfo dashboardInfo1 = null;
+            string embedConfigPath = Server.MapPath("~/embedConfig.json");
+            string jsonString = System.IO.File.ReadAllText(embedConfigPath);
+            GlobalAppSettings.EmbedDetails = JsonConvert.DeserializeObject<EmbedDetails>(jsonString);
 
-        }
+
+            // Check for null to avoid potential issues
+            if (GlobalAppSettings.EmbedDetails != null)
+            {
+                // Create an instance of DashboardInfo and set properties
+                 dashboardInfo1 = new DashboardInfo
+                {
+                    DashboardId = GlobalAppSettings.EmbedDetails.DashboardId,
+                    SiteIdentifier = GlobalAppSettings.EmbedDetails.SiteIdentifier,
+                    ServerUrl = GlobalAppSettings.EmbedDetails.ServerUrl,
+                    EmbedType = GlobalAppSettings.EmbedDetails.EmbedType,
+                    Environment = GlobalAppSettings.EmbedDetails.Environment
+                };
+
+            }
+            HttpContext.Current.Items["DashboardInfo"] = dashboardInfo1;
+            // Retrieve the dashboardInfo from HttpContext.Current.Items
+            var dashboardInfo = HttpContext.Current.Items["DashboardInfo"] as DashboardInfo;
+
+                if (dashboardInfo != null)
+                {
+                    // Serialize the dashboardInfo to JSON
+                    return JsonConvert.SerializeObject(dashboardInfo);
+                }
+                else
+                {
+                    // Return an empty object if dashboardInfo is null
+                    return "{}";
+                }
+            }
+        //}
     }
 }
